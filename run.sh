b679ac5e6039184c134cab48f716e71ab7db6d2f
Josh1st <hugostiglits@gmail.com>
2023-05-08 14:16:23 +0200

    removed debug

--- run.sh
+++ run.sh
@@ -6,5 +6,4 @@ export FLASK_APP=rembg
 
 pip install -r requirements.txt
 
-# functions-framework --target rembg --debug --port=8080
-python env/lib/python3.10/site-packages/functions_framework/__main__.py --target rembg --debug --port=8080
+python env/lib/python3.10/site-packages/functions_framework/__main__.py --target rembg --port=8080
